21e4f06de4d46d582905dd3f09f7167fe1b8b642
INDERJEET <149118888+Inderjeet1Singh@users.noreply.github.com>
2024-06-26 00:18:54 +0530

    Create Hero.jsx

---
+++ frontend/src/omponents/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hero = ({title,imageUrl}) => {
+  return (
+    <div className="hero container">
+      <div className="banner">
+        <h1>{title}</h1>
+        <p>
+        "Discover personalized care at DocDiv.
+         We tailor our treatments to fit your unique needs and lifestyle. 
+         Experience the difference that individualized attention can make."
+        </p>
+      </div>
+      <div className="banner">
+        <img src={imageUrl} alt ="hero" className = "animated-image"/>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
